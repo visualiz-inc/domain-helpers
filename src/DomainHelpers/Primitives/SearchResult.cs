4c111f2f3f53ed2639b4b794af477191a6a03feb
rempei-okada <okada@visualiz.jp>
2022-09-02 10:24:31 +0900

    エラー修正

--- src/DomainHelpers/Primitives/SearchResult.cs
+++ src/DomainHelpers/Primitives/SearchResult.cs
@@ -1,23 +1,41 @@
-﻿namespace DomainHelpers.Primitives {
-    public abstract class SearchResult {
-        public static SearchResult<T> From<T>(
-            int hitCount,
-            int offset,
-            int fetch,
-            ImmutableArray<T> items
-        ) {
-            return new(
-                hitCount,
-                offset,
-                fetch,
-                items
-            );
-        }
-    }
+﻿using System.Diagnostics.CodeAnalysis;
 
-    public record SearchResult<T>(
-        int HitCount,
-        int Offset,
-        int Fetch,
-        ImmutableArray<T> Items) { }
+namespace DomainHelpers.Primitives;
+
+public abstract class SearchResult {
+    public static SearchResult<T> From<T>(
+        int hitCount,
+        int offset,
+        int fetch,
+        ImmutableArray<T> items
+    ) {
+        return new() {
+            HitCount = hitCount, Offset = offset, Fetch = fetch, Items = items,
+        };
+    }
+}
+
+public record SearchResult<T> {
+    public required int HitCount { get; init; }
+
+    public required int Offset { get; init; }
+
+    public required int Fetch { get; init; }
+
+    public required ImmutableArray<T> Items { get; init; }
+
+    public SearchResult() { }
+
+    [SetsRequiredMembers]
+    public SearchResult(
+        int hitCount,
+        int offset,
+        int fetch,
+        ImmutableArray<T> items
+    ) {
+        this.HitCount = hitCount;
+        this.Items = items;
+        this.Fetch = fetch;
+        this.Offset = offset;
+    }
 }
