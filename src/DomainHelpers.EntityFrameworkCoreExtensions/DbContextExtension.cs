412c2cc392fa91111da7279ce9f1a0e2ea2d423e
rempei-okada <okada@visualiz.jp>
2023-04-04 15:55:18 +0900

    ヘルパー追加

---
+++ src/DomainHelpers.EntityFrameworkCoreExtensions/DbContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainHelpers.EntityFrameworkCoreExtensions;
+
+public static class DbContextExtension {
+    public static async Task<TEntity?> FindByIdAndDetachAsync<TEntity>(this DbSet<TEntity> dbset, string id) where TEntity : class {
+        if (await dbset.FindAsync(id) is { } e) {
+            dbset.Entry(e).State = EntityState.Detached;
+            return e;
+        }
+
+        return null;
+    }
+}
