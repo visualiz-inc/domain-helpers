f1eca3ed1f3a3e184f031f26ac0bb3c888d109a1
rempei-okada <okada@visualiz.jp>
2023-04-04 21:03:45 +0900

    実装（動作未チェック）

---
+++ src/DomainHelpers.EntityFrameworkCoreExtensions/OrderByExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainHelpers.EntityFrameworkCoreExtensions;
+
+public enum SortDirection {
+    None,
+    Asc,
+    Desc,
+}
+
+public static class OrderByExtensions {
+    public static IQueryable<T> Sort<T,TSortProperty>(this IQueryable< T> query,IEnumerable<(TSortProperty, SortDirection)> sorts) {
+
+        var q = query;
+        foreach(var s in sorts) {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = Expression.Property(parameter, "name");
+            var orderByLambda = Expression.Lambda<Func<T, object>>(Expression.Convert(property, typeof(object)), parameter);
+
+            q = q.OrderBy(orderByLambda);
+        }
+
+        return q;
+    }
+}
