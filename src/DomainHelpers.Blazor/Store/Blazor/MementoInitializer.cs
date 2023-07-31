71157192de43131ce43914aca228f5245c05e2d7
rempei-okada <okada@visualiz.jp>
2023-07-31 14:59:28 +0900

    初期化の通知

--- src/DomainHelpers.Blazor/Store/Blazor/MementoInitializer.cs
+++ src/DomainHelpers.Blazor/Store/Blazor/MementoInitializer.cs
@@ -8,10 +8,14 @@ public class MementoInitializer : ComponentBase, IDisposable {
     [Inject]
     public required StoreProvider StoreProvider { get; set; }
 
+    [Parameter]
+    public EventCallback Initialized { get; set; }
+
     protected override async Task OnAfterRenderAsync(bool firstRender) {
         await base.OnAfterRenderAsync(firstRender);
         if (firstRender) {
             await StoreProvider.InitializeAsync();
+            await Initialized.InvokeAsync();
         }
     }
 
