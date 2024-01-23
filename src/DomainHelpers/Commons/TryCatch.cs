62280f03e2ac8c51fc3943366cc40e566f02c1d7
rempei-okada <okada@visualiz.jp>
2024-01-23 18:10:05 +0900

    リファクタとパッケージ更新

--- src/DomainHelpers/Commons/TryCatch.cs
+++ src/DomainHelpers/Commons/TryCatch.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DomainHelpers.Commons;
+﻿namespace DomainHelpers.Commons;
 
 public class TryCatchUtils {
     public static T? Try<T>(Func<T> func) {
